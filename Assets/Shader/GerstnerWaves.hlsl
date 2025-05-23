de27e75b365ebf7a409784af1c88c34583356166
Lucia Fornos <lucia.fornos4@gmail.com>
2025-05-23 19:30:07 +0200

    [GERSTNER] steepness removed

--- Assets/Shader/GerstnerWaves.hlsl
+++ Assets/Shader/GerstnerWaves.hlsl
@@ -4,7 +4,6 @@ void GerstnerWaves_float(
     float amplitude,
     float wavelength,
     float speed,
-    float steepness,
     float time,
     float phase,
     out float3 offset,
@@ -12,25 +11,30 @@ void GerstnerWaves_float(
 {
     float2 D = normalize(direction);
     float k = 2.0 * 3.14159265 / wavelength;
-    float phi = k * dot(D, position.xz) - speed * k * time + phase;
+    float phi = k * dot(D, position.xz)
+                - speed * k * time
+                + phase;
 
     float cosPhi = cos(phi);
     float sinPhi = sin(phi);
 
-    offset.x = steepness * amplitude * D.x * cosPhi;
-    offset.z = steepness * amplitude * D.y * cosPhi;
+    // desplazamiento sin steepness
+    offset.x = amplitude * D.x * cosPhi;
     offset.y = amplitude * sinPhi;
-
+    offset.z = amplitude * D.y * cosPhi;
+    
+    // tangente
     float3 tangent = float3(
-        1.0 - steepness * amplitude * k * D.x * D.x * sinPhi,
-        steepness * amplitude * k * D.x * cosPhi,
-        -steepness * amplitude * k * D.x * D.y * sinPhi
+        1.0 - amplitude * k * D.x * D.x * sinPhi,
+        amplitude * k * D.x * cosPhi,
+       -amplitude * k * D.x * D.y * sinPhi
     );
 
+    // binormal
     float3 binormal = float3(
-        -steepness * amplitude * k * D.x * D.y * sinPhi,
-        steepness * amplitude * k * D.y * cosPhi,
-        1.0 - steepness * amplitude * k * D.y * D.y * sinPhi
+       -amplitude * k * D.x * D.y * sinPhi,
+        amplitude * k * D.y * cosPhi,
+        1.0 - amplitude * k * D.y * D.y * sinPhi
     );
 
     normal = normalize(cross(binormal, tangent));
